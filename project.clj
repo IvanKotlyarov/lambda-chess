2d11d3f4f04cfb6ffef2200b1b7d9bea8cbc6bef
Ivan Kotlyarov <ivan.kotlyarov@gmail.com>
2022-02-26 20:46:17 +0300

    chore: Ready for Haskell

--- project.clj
+++
@@ -1,9 +0,0 @@
-(defproject lambda-chess "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"] [org.clojure/math.combinatorics "0.1.6"] [com.clojure-goes-fast/clj-async-profiler "1.0.0-alpha1"]
-                 ]
-  :repl-options {:init-ns lambda-chess.core}
-  :jvm-opts ["-Djdk.attach.allowAttachSelf"])
